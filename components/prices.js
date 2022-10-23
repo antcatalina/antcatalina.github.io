dbbcd2f5965b9714c444190c6e2bd4c48afdcad2
Anthony <antcatalina1@gmail.com>
2022-10-23 14:04:40 -0400

    Added more coins, fixed spacing/mobile

--- components/prices.js
+++ components/prices.js
@@ -1,7 +1,9 @@
 var btc = document.getElementById("bitcoin");
-var ltc = document.getElementById("ethereum");
+var eth = document.getElementById("ethereum");
 var ada = document.getElementById("cardano");
 var xrp = document.getElementById("ripple");
+var eos = document.getElementById("eos");
+var ltc = document.getElementById("litecoin");
 var btcUp = document.getElementById("btc-up");
 var btcDown = document.getElementById("btc-down");
 var btcUpArrow = document.getElementById("btc-up-arrow");
@@ -18,11 +20,19 @@ var xrpUp = document.getElementById("xrp-up");
 var xrpDown = document.getElementById("xrp-down");
 var xrpUpArrow = document.getElementById("xrp-up-arrow");
 var xrpDownArrow = document.getElementById("xrp-down-arrow");
+var eosUp = document.getElementById("eos-up");
+var eosDown = document.getElementById("eos-down");
+var eosUpArrow = document.getElementById("eos-up-arrow");
+var eosDownArrow = document.getElementById("eos-down-arrow");
+var ltcUp = document.getElementById("ltc-up");
+var ltcDown = document.getElementById("ltc-down");
+var ltcUpArrow = document.getElementById("ltc-up-arrow");
+var ltcDownArrow = document.getElementById("ltc-down-arrow");
 
 var liveprice = {
     "async": true,
     "scroosDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano%2Cripple&vs_currencies=usd&include_24hr_change=true",
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano%2Cripple%2Ceos%2Clitecoin&vs_currencies=usd&include_24hr_change=true",
 
     "method": "GET",
     "headers": {}
@@ -30,9 +40,11 @@ var liveprice = {
 
 $.ajax(liveprice).done(function (response){
     btc.innerHTML = response.bitcoin.usd;
-    ltc.innerHTML = response.ethereum.usd;
+    eth.innerHTML = response.ethereum.usd;
     ada.innerHTML = response.cardano.usd;
     xrp.innerHTML = response.ripple.usd;
+    eos.innerHTML = response.eos.usd;
+    ltc.innerHTML = response.litecoin.usd;
     let btcTrend = response.bitcoin.usd_24h_change;
     if (btcTrend > 0) {
         btcUp.innerHTML = btcTrend.toFixed(3) + "%"
@@ -68,6 +80,24 @@ $.ajax(liveprice).done(function (response){
     else {
         xrpDown.innerHTML = xrpTrend.toFixed(3) + "%"
         xrpDownArrow.style.display = 'block';
+    }
+    let eosTrend = response.eos.usd_24h_change;
+    if (eosTrend > 0) {
+        eosUp.innerHTML = eosTrend.toFixed(3) + "%";
+        eosUpArrow.style.display = 'block';
+    }
+    else {
+        eosDown.innerHTML = eosTrend.toFixed(3) + "%"
+        eosDownArrow.style.display = 'block';
+    }
+    let ltcTrend = response.litecoin.usd_24h_change;
+    if (ltcTrend > 0) {
+        ltcUp.innerHTML = ltcTrend.toFixed(3) + "%";
+        ltcUpArrow.style.display = 'block';
+    }
+    else {
+        ltcDown.innerHTML = ltcTrend.toFixed(3) + "%"
+        ltcDownArrow.style.display = 'block';
     }
 
 });
